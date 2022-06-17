f32e64b2d21c0dcce7927a01785fdcb0d758fbcc
MOON <y005@naver.com>
2022-06-17 18:12:43 +0900

    init project

---
+++ src/main/java/com/example/woowa/WoowaApplication.java
@@ -0,0 +1,13 @@
+package com.example.woowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WoowaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/woowa/WoowaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.woowa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoowaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
