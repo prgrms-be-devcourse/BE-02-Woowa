b32deacd451b71adc74ec1bdfdc58a686294704e
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-06-20 15:18:17 +0900

    Revert "[#1] - 배달관련 기본 도메인 개발" (#7)

--- src/main/java/com/example/woowa/rider/repository/DeliveryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.woowa.rider.repository;
-
-import com.example.woowa.rider.entity.Delivery;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
-}
