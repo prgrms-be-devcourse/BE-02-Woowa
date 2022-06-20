c33b9a483098d577fc8c8b0a50569d3eb3ff2e29
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-06-20 14:55:32 +0900

    feat(DeliveryArea) : lombok 생성자 추가

--- src/main/java/com/example/woowa/rider/entity/DeliveryArea.java
+++ src/main/java/com/example/woowa/rider/entity/DeliveryArea.java
@@ -1,12 +1,17 @@
 package com.example.woowa.rider.entity;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Table
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class DeliveryArea {
 
     @Id
