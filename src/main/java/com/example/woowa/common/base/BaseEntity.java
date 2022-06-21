b32a473eb1f585c28a672c5634ff7542246fdbab
chlwlgns524 <chlwlgns524@naver.com>
2022-06-21 21:59:23 +0900

    feat: BaseEntity entity 정의

---
+++ src/main/java/com/example/woowa/common/base/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.example.woowa.common.base;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+}
