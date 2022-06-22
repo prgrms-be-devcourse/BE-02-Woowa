f23856fe8cef2c9db437765d93c11d46b1aaa266
chlwlgns524 <chlwlgns524@naver.com>
2022-06-22 20:43:45 +0900

    refactor: BaseEntity에 @CreatedDate와 @LastModifiedDate를 이용한 자동화 설정 및 해당 기능(auditing) enable을 위해 springboot application에 @EnableJpaAuditing 추가

--- src/main/java/com/example/woowa/common/base/BaseEntity.java
+++ src/main/java/com/example/woowa/common/base/BaseEntity.java
@@ -2,23 +2,25 @@ package com.example.woowa.common.base;
 
 import java.time.LocalDateTime;
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
+    @CreatedDate
     @Column(nullable = false, updatable = false)
-    private final LocalDateTime createdAt;
+    private LocalDateTime createdAt;
 
+    @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime updatedAt;
-
-    public BaseEntity() {
-        createdAt = LocalDateTime.now();
-        updatedAt = LocalDateTime.now();
-    }
 
     public void changeUpdatedAt(LocalDateTime updatedAt) {
         this.updatedAt = updatedAt;
