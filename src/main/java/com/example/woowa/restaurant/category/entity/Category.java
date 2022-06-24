35ca2f89c48a086f92bfd0db428953a89020ec47
chlwlgns524 <chlwlgns524@naver.com>
2022-06-24 21:43:17 +0900

    fix: Category entity 중간 테이블과 양방향 맵핑으로 변경 및 연관 관계 편의 메소드 추가

--- src/main/java/com/example/woowa/restaurant/category/entity/Category.java
+++ src/main/java/com/example/woowa/restaurant/category/entity/Category.java
@@ -1,10 +1,15 @@
 package com.example.woowa.restaurant.category.entity;
 
+import com.example.woowa.restaurant.restaurntat_category.entity.RestaurantCategory;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AccessLevel;
@@ -21,11 +26,20 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<RestaurantCategory> restaurantCategories = new ArrayList<>();
+
     @Column(unique = true, nullable = false, length = 10)
     private String name;
 
     public Category(String name) {
         this.name = name;
+    }
+
+    public void addRestaurantCategory(RestaurantCategory restaurantCategory) {
+        if (restaurantCategory.getCategory() != this) {
+            restaurantCategory.setCategory(this);
+        }
     }
 
     public void changeCategoryName(String name) {
