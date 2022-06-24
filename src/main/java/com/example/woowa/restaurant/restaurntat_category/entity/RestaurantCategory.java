d649fdce851da8ed8dc6af51b72cc3ac13b61e50
chlwlgns524 <chlwlgns524@naver.com>
2022-06-24 19:08:11 +0900

    fix: ManyToOne 어노테이션에 nullable = false 추가 및 배달가게와 카테고리 각각에 대한 연관 관계 편의 …

--- src/main/java/com/example/woowa/restaurant/restaurntat_category/entity/RestaurantCategory.java
+++ src/main/java/com/example/woowa/restaurant/restaurntat_category/entity/RestaurantCategory.java
@@ -3,6 +3,7 @@ package com.example.woowa.restaurant.restaurntat_category.entity;
 import com.example.woowa.restaurant.category.entity.Category;
 import com.example.woowa.restaurant.restaurant.entity.Restaurant;
 
+import java.util.Objects;
 import javax.persistence.*;
 
 import lombok.AccessLevel;
@@ -20,12 +21,28 @@ public class RestaurantCategory {
 
     @Id
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "restaurant_id")
+    @JoinColumn(name = "restaurant_id", nullable = false)
     private Restaurant restaurant;
 
     @Id
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id")
+    @JoinColumn(name = "category_id", nullable = false)
     private Category category;
+
+    public void setRestaurant(Restaurant restaurant) {
+        if (Objects.nonNull(this.restaurant)) {
+            this.restaurant.getRestaurantCategories().remove(this);
+        }
+        this.restaurant = restaurant;
+        this.restaurant.getRestaurantCategories().add(this);
+    }
+
+    public void setCategory(Category category) {
+        if (Objects.nonNull(this.category)) {
+            this.category.getRestaurantCategories().remove(this);
+        }
+        this.category = category;
+        this.category.getRestaurantCategories().add(this);
+    }
 
 }
