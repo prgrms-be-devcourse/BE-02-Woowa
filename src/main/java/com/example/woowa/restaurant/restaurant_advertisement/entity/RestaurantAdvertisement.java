d7a549b46403d6229b12724eb53ea858aac543c5
chlwlgns524 <chlwlgns524@naver.com>
2022-06-24 17:49:29 +0900

    fix: ManyToOne 어노테이션에 nullable = false 추가 및 배달가게와 광고 각각에 대한 연관 관계 편의 메소드 작성

--- src/main/java/com/example/woowa/restaurant/restaurant_advertisement/entity/RestaurantAdvertisement.java
+++ src/main/java/com/example/woowa/restaurant/restaurant_advertisement/entity/RestaurantAdvertisement.java
@@ -2,14 +2,14 @@ package com.example.woowa.restaurant.restaurant_advertisement.entity;
 
 import com.example.woowa.restaurant.advertisement.entity.Advertisement;
 import com.example.woowa.restaurant.restaurant.entity.Restaurant;
-
+import java.util.Objects;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,19 +18,35 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@IdClass(RestaurantAdvertisementId.class)
+@IdClass(RestaurnatAdvertisementId.class)
 @Table(name = "restaurant_advertisement")
 @Entity
 public class RestaurantAdvertisement {
 
     @Id
-    @ManyToOne
-    @JoinColumn(name = "restaurant_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false)
     private Restaurant restaurant;
 
     @Id
-    @ManyToOne
-    @JoinColumn(name = "advertisement_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "advertisement_id", nullable = false)
     private Advertisement advertisement;
+
+    public void setRestaurant(Restaurant restaurant) {
+        if (Objects.nonNull(this.restaurant)) {
+            this.restaurant.getRestaurantAdvertisements().remove(this);
+        }
+        this.restaurant = restaurant;
+        this.restaurant.getRestaurantAdvertisements().add(this);
+    }
+
+    public void setAdvertisement(Advertisement advertisement) {
+        if (Objects.nonNull(this.advertisement)) {
+            this.advertisement.getRestaurantAdvertisements().remove(this);
+        }
+        this.advertisement = advertisement;
+        this.advertisement.addRestaurantAdvertisement(this);
+    }
 
 }
