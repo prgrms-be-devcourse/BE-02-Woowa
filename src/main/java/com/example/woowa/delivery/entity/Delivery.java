26bf0011f3af6b30b885f1ea6dbea99fddfca35a
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-06-26 22:48:35 +0900

    feat : Delivery 배달료 컬럼 추가, 연관관계 추가, 생성자 추가.

--- src/main/java/com/example/woowa/delivery/entity/Delivery.java
+++ src/main/java/com/example/woowa/delivery/entity/Delivery.java
@@ -1,6 +1,9 @@
 package com.example.woowa.delivery.entity;
 
+import com.example.woowa.customer.customer.entity.Customer;
 import com.example.woowa.delivery.enums.DeliveryStatus;
+import com.example.woowa.order.order.entity.Order;
+import com.example.woowa.restaurant.restaurant.entity.Restaurant;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,10 +27,31 @@ public class Delivery {
     @Column(nullable = false)
     private String customerAddress;
 
+    @Column(nullable = false)
+    private int deliveryFee;
+
     @Enumerated(EnumType.STRING)
     @Column(columnDefinition = "BOOLEAN DEFAULT FALSE")
     private DeliveryStatus deliveryStatus;
 
     @Column(nullable = false)
     private LocalDate arrivalTime;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Order order;
+
+    private Delivery(String restaurantAddress, String customerAddress, int deliveryFee, DeliveryStatus deliveryStatus) {
+        this.restaurantAddress = restaurantAddress;
+        this.customerAddress = customerAddress;
+        this.deliveryFee = deliveryFee;
+        this.deliveryStatus = deliveryStatus;
+    }
+
+    public static Delivery createDelivery(Order order, String restaurantAddress, String customerAddress, int deliveryFee) {
+        return new Delivery(restaurantAddress, customerAddress, deliveryFee, DeliveryStatus.DELIVERY_WAITING);
+    }
+
+    private void changeDeliveryStatus(DeliveryStatus deliveryStatus) {
+        this.deliveryStatus = deliveryStatus;
+    }
 }
