a2df65933db8d45bef39ae6ad2f5253570fda9fb
chlwlgns524 <chlwlgns524@naver.com>
2022-06-27 15:39:21 +0900

    fix: RateType에 함수형으로 수정

--- src/main/java/com/example/woowa/restaurant/advertisement/enums/RateType.java
+++ src/main/java/com/example/woowa/restaurant/advertisement/enums/RateType.java
@@ -1,31 +1,21 @@
 package com.example.woowa.restaurant.advertisement.enums;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum RateType {
 
-    FLAT("정액제") {
-        @Override
-        public int applyRate(int totalPrice, int rate) {
-            return totalPrice - rate;
-        }
-    },
-    PERCENT("비율제") {
-        @Override
-        public int applyRate(int totalPrice, int rate) {
-            return (int)Math.round(totalPrice * (1 - rate / 100.0));
-        }
-    };
+    FLAT("정액제", (price, rate) -> price - rate),
+    PERCENT("비율제", (price, rate) -> (int) (price * (1 - rate / 100.0)));
 
     private final String name;
+    private final BiFunction<Integer, Integer, Integer> apply;
 
     public String getName() {
         return name;
     }
-
-    public abstract int applyRate(int totalPrice, int rate);
 
     public static RateType getRateType(String name) {
         return Arrays.stream(values())
