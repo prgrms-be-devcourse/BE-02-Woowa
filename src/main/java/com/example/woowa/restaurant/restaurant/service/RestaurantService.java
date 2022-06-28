454aafd29a1ca2f0558b6e4ceb5d7f4fcaedf158
chlwlgns524 <chlwlgns524@naver.com>
2022-06-28 14:17:31 +0900

    fix: 가게(Restaurant) 엔티티 연관 관계 맵핑에 따른 메소드 추가

--- src/main/java/com/example/woowa/restaurant/restaurant/service/RestaurantService.java
+++ src/main/java/com/example/woowa/restaurant/restaurant/service/RestaurantService.java
@@ -1,8 +1,15 @@
 package com.example.woowa.restaurant.restaurant.service;
 
+import com.example.woowa.restaurant.category.entity.Category;
+import com.example.woowa.restaurant.category.service.CategoryService;
 import com.example.woowa.restaurant.restaurant.entity.Restaurant;
 import com.example.woowa.restaurant.restaurant.repository.RestaurantRepository;
+import com.example.woowa.restaurant.restaurntat_category.entity.RestaurantCategory;
+import com.example.woowa.restaurant.restaurntat_category.entity.RestaurantCategoryId;
+import com.example.woowa.restaurant.restaurntat_category.repository.RestaurantCategoryRepository;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +20,24 @@ import org.springframework.transaction.annotation.Transactional;
 public class RestaurantService {
 
     private final RestaurantRepository restaurantRepository;
+    private final RestaurantCategoryRepository restaurantCategoryRepository;
+    private final CategoryService categoryService;
+
+    @Transactional
+    public Long createRestaurant(String name, String businessNumber, LocalTime openingTime,
+        LocalTime closingTime, Boolean isOpen, String phoneNumber, String description,
+        String address, List<Long> categories) {
+        Restaurant restaurant = restaurantRepository.save(
+            Restaurant.createRestaurant(name, businessNumber, openingTime, closingTime,
+                isOpen, phoneNumber, description, address));
+
+        categories.forEach(categoryId -> {
+            Category category = categoryService.findCategoryById(categoryId);
+            RestaurantCategory restaurantCategory = new RestaurantCategory(restaurant, category);
+        });
+
+        return restaurant.getId();
+    }
 
     public Restaurant findRestaurantById(Long restaurantId) {
         return restaurantRepository.findById(restaurantId)
@@ -50,4 +75,31 @@ public class RestaurantService {
     public void changeRestaurantPhoneNumber(Long restaurantId, String newPhoneNumber) {
         findRestaurantById(restaurantId).changePhoneNumber(newPhoneNumber);
     }
+
+    @Transactional
+    public void addCategory(Long restaurantId, Long categoryId) {
+        Restaurant restaurant = findRestaurantById(restaurantId);
+        Category category = categoryService.findCategoryById(categoryId);
+
+        RestaurantCategory restaurantCategory = new RestaurantCategory(restaurant, category);
+    }
+
+    @Transactional
+    public void removeCategory(Long restaurantId, Long categoryId) {
+        Restaurant restaurant = findRestaurantById(restaurantId);
+        Category category = categoryService.findCategoryById(categoryId);
+
+        RestaurantCategory restaurantCategory = restaurantCategoryRepository.findById(
+            new RestaurantCategoryId(restaurantId, categoryId)).get();
+
+        restaurant.getRestaurantCategories().remove(restaurantCategory);
+        category.getRestaurantCategories().remove(restaurantCategory);
+    }
+
+    public List<Category> findCategories(Long restaurantId) {
+        return findRestaurantById(restaurantId).getRestaurantCategories().stream()
+            .map(RestaurantCategory::getCategory)
+            .collect(Collectors.toList());
+    }
+
 }
