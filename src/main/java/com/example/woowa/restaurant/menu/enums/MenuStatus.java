404fb0dc0cc9b5eb12ef888921567c6dad1a81ad
jeonghaemin <jhm0828@gmail.com>
2022-06-29 22:48:36 +0900

    feat: MenuStatus 직렬화/역직렬화에 사용될 필드 추가

--- src/main/java/com/example/woowa/restaurant/menu/enums/MenuStatus.java
+++ src/main/java/com/example/woowa/restaurant/menu/enums/MenuStatus.java
@@ -1,5 +1,31 @@
 package com.example.woowa.restaurant.menu.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import java.util.Arrays;
+
 public enum MenuStatus {
-    SALE, SOLD_OUT, HIDDEN
+    SALE("sale", "판매중"), SOLD_OUT("sold_out", "품절"), HIDDEN("hidden", "숨김");
+
+    private final String code;
+    private final String viewName;
+
+    MenuStatus(String code, String viewName) {
+        this.code = code;
+        this.viewName = viewName;
+    }
+
+    @JsonValue
+    public String getViewName() {
+        return viewName;
+    }
+
+    @JsonCreator
+    public static MenuStatus find(@JsonProperty("menuStatus") String code) {
+        return Arrays.stream(MenuStatus.values())
+                .filter(status -> status.code.equals(code))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("code와 일치하는 타입이 없습니다."));
+    }
 }
