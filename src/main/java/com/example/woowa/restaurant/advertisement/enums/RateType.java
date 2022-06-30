c6f280059cfbabbae2140da4e867b40744c3709c
chlwlgns524 <chlwlgns524@naver.com>
2022-06-30 18:38:06 +0900

    fix: EnumFindable 인터페이스 구현으로 인한 코드 수정

--- src/main/java/com/example/woowa/restaurant/advertisement/enums/RateType.java
+++ src/main/java/com/example/woowa/restaurant/advertisement/enums/RateType.java
@@ -1,11 +1,11 @@
 package com.example.woowa.restaurant.advertisement.enums;
 
-import java.util.Arrays;
+import com.example.woowa.common.EnumFindable;
 import java.util.function.BiFunction;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public enum RateType {
+public enum RateType implements EnumFindable {
 
     FLAT("정액제", (price, rate) -> price - rate),
     PERCENT("비율제", (price, rate) -> (int) (price * (1 - rate / 100.0)));
@@ -13,15 +13,9 @@ public enum RateType {
     private final String name;
     private final BiFunction<Integer, Integer, Integer> apply;
 
-    public String getName() {
-        return name;
-    }
-
-    public static RateType getRateType(String name) {
-        return Arrays.stream(values())
-            .filter(rateType -> rateType.getName().equals(name))
-            .findFirst()
-            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 요금제입니다."));
+    @Override
+    public String getType() {
+        return this.name;
     }
 
 }
