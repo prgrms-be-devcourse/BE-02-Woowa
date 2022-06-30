2d0377a6e692f43b8818c5f691016efa13b876ba
chlwlgns524 <chlwlgns524@naver.com>
2022-06-30 15:30:25 +0900

    feat: 카테고리(Category) 컨트롤러 개발

---
+++ src/main/java/com/example/woowa/restaurant/category/controller/CategoryController.java
@@ -0,0 +1,72 @@
+package com.example.woowa.restaurant.category.controller;
+
+import com.example.woowa.restaurant.category.dto.request.CategoryUpdateRequest;
+import com.example.woowa.restaurant.category.dto.response.CategoryCreateResponse;
+import com.example.woowa.restaurant.category.dto.response.CategoryFindResponse;
+import com.example.woowa.restaurant.category.service.CategoryService;
+import com.example.woowa.restaurant.restaurant.dto.response.RestaurantFindResponse;
+import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RequiredArgsConstructor
+@RequestMapping(value = "baemin/v1/categories")
+@RestController
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CategoryCreateResponse> createCategory(@RequestParam
+    @Size(min = 1, max = 10, message = "카테고리명은 1자 이상 10자이하여야 합니다.") String name) {
+        CategoryCreateResponse newCategory = categoryService.createCategory(name);
+        return new ResponseEntity<>(newCategory, HttpStatus.OK);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<CategoryFindResponse>> findAllCategories() {
+        List<CategoryFindResponse> categories = categoryService.findCategories();
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "{categoryId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CategoryFindResponse> findCategory(@PathVariable Long categoryId) {
+        CategoryFindResponse category = categoryService.findCategoryById(categoryId);
+        return new ResponseEntity<>(category, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "{categoryId}/restaurants", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<RestaurantFindResponse>> findRestaurantByCategoryId(@PathVariable Long categoryId) {
+        List<RestaurantFindResponse> restaurants = categoryService.findRestaurantsByCategoryId(categoryId);
+        return new ResponseEntity<>(restaurants, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{categoryId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> changeCategoryName(@PathVariable Long categoryId,
+        @RequestBody @Valid CategoryUpdateRequest categoryUpdateRequest) {
+        categoryService.changeCategoryName(categoryId, categoryUpdateRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{categoryId}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long categoryId) {
+        categoryService.deleteCategory(categoryId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
