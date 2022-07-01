8231e0ee1f862164b454e49f322ffc37d4f20890
chlwlgns524 <chlwlgns524@naver.com>
2022-07-01 16:24:49 +0900

    feat: 광고(Advertisement) response DTO 정의

---
+++ src/main/java/com/example/woowa/restaurant/advertisement/dto/response/AdvertisementFindResponse.java
@@ -0,0 +1,31 @@
+package com.example.woowa.restaurant.advertisement.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class AdvertisementFindResponse {
+
+    private final Long id;
+
+    private final String title;
+
+    private final String unitType;
+
+    private final String rateType;
+
+    private final Integer rate;
+
+    private final String description;
+
+    private final Integer limitSize;
+
+    private final Integer currentSize;
+
+    private final LocalDateTime createdAt;
+
+    private final LocalDateTime updatedAt;
+
+}
