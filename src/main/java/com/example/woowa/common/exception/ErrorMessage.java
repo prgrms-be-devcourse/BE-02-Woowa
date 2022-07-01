02ae298d0ced02e2292c9b85824b5c7ff8cfcba6
jeonghaemin <jhm0828@gmail.com>
2022-07-02 00:09:45 +0900

    feat: 배달 지역 서비스에 레스토랑의 특정 지역 배달비 조회 메서드 추가

--- src/main/java/com/example/woowa/common/exception/ErrorMessage.java
+++ src/main/java/com/example/woowa/common/exception/ErrorMessage.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorMessage {
     INVALID_INPUT_VALUE("잘못된 데이터를 입력하였습니다."),
-    INVALID_PERIOD_VALUE("조회 기간의 시작일은 마감일 이전이어야 합니다.");
+    INVALID_PERIOD_VALUE("조회 기간의 시작일은 마감일 이전이어야 합니다."),
+
+    NOT_ORDERABLE_AREA("배달 가능 지역이 아닙니다.");
 
     private final String message;
 }
--- src/main/java/com/example/woowa/delivery/service/DeliveryAreaService.java
+++ src/main/java/com/example/woowa/delivery/service/DeliveryAreaService.java
@@ -1,13 +1,11 @@
 package com.example.woowa.delivery.service;
 
-import com.example.woowa.delivery.entity.AreaCode;
-import com.example.woowa.delivery.entity.Delivery;
+import com.example.woowa.common.exception.ErrorMessage;
 import com.example.woowa.delivery.entity.DeliveryArea;
 import com.example.woowa.delivery.repository.DeliveryAreaRepository;
+import com.example.woowa.restaurant.restaurant.entity.Restaurant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +15,13 @@ public class DeliveryAreaService {
 
     public void save(DeliveryArea deliveryArea) {
         deliveryAreaRepository.save(deliveryArea);
+    }
+
+    public int getDeliveryFee(Restaurant restaurant, String defaultAddress) {
+        return deliveryAreaRepository.findByRestaurantAndAddress(restaurant, defaultAddress)
+                .orElseThrow(() -> new IllegalArgumentException(
+                        ErrorMessage.NOT_ORDERABLE_AREA.getMessage()))
+                .getDeliveryFee();
     }
 
 }
