f86b106d7e66989bb0748901d55dac6897764a3d
chlwlgns524 <chlwlgns524@naver.com>
2022-07-04 11:47:46 +0900

    fix: 사장님(Owner) 서비스 map struct 및 security 반영

--- src/main/java/com/example/woowa/restaurant/owner/service/OwnerService.java
+++ src/main/java/com/example/woowa/restaurant/owner/service/OwnerService.java
@@ -1,12 +1,25 @@
 package com.example.woowa.restaurant.owner.service;
 
+import com.example.woowa.restaurant.owner.dto.request.OwnerCreateRequest;
+import com.example.woowa.restaurant.owner.dto.request.OwnerUpdateRequest;
+import com.example.woowa.restaurant.owner.dto.response.OwnerCreateResponse;
+import com.example.woowa.restaurant.owner.dto.response.OwnerFindResponse;
 import com.example.woowa.restaurant.owner.entity.Owner;
+import com.example.woowa.restaurant.owner.mapper.OwnerMapper;
 import com.example.woowa.restaurant.owner.repository.OwnerRepository;
+import com.example.woowa.restaurant.restaurant.dto.request.RestaurantCreateRequest;
+import com.example.woowa.restaurant.restaurant.dto.response.RestaurantFindResponse;
 import com.example.woowa.restaurant.restaurant.entity.Restaurant;
+import com.example.woowa.restaurant.restaurant.mapper.RestaurantMapper;
 import com.example.woowa.restaurant.restaurant.service.RestaurantService;
-import java.util.ArrayList;
+import com.example.woowa.security.repository.RoleRepository;
+import com.example.woowa.security.repository.UserRepository;
+import com.example.woowa.security.user.User;
+import com.example.woowa.security.user.UserRole;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,71 +28,71 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class OwnerService {
 
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+
     private final OwnerRepository ownerRepository;
     private final RestaurantService restaurantService;
+    private final OwnerMapper ownerMapper;
+    private final RestaurantMapper restaurantMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public Long createOwner(String loginId, String loginPassword, String name, String phoneNumber) {
-        return ownerRepository.save(new Owner(loginId, loginPassword, name, phoneNumber)).getId();
+    public OwnerCreateResponse createOwner(OwnerCreateRequest ownerCreateRequest) {
+        Owner owner = ownerRepository.save(ownerMapper.toEntity(ownerCreateRequest));
+        owner.changePassword(passwordEncoder.encode(owner.getPassword()));
+        userRepository.save(new User(owner.getLoginId(), owner.getPassword(), owner.getName(), owner.getPhoneNumber(),
+            List.of(roleRepository.findByName(UserRole.ROLE_OWNER.toString()))));
+        return ownerMapper.toCreateResponse(owner);
     }
 
-    public List<Owner> findOwners() {
-        return ownerRepository.findAll();
+    public List<OwnerFindResponse> findOwners() {
+        return ownerRepository.findAll().stream()
+            .map(ownerMapper::toFindResponse)
+            .collect(Collectors.toList());
     }
 
-    public Owner findOwnerById(Long ownerId) {
-        return ownerRepository.findById(ownerId)
-            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사장님 아이디입니다."));
+    public OwnerFindResponse findOwnerById(Long ownerId) {
+        return ownerMapper.toFindResponse(findOwnerEntityById(ownerId));
     }
 
-    public Owner findOwnerByLoginId(String loginId) {
-        return ownerRepository.findOwnerByLoginId(loginId)
-            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사장님 로그인 아이디입니다."));
-    }
-
-    public List<Restaurant> findRestaurantsByOwnerId(Long ownerId) {
-        return new ArrayList<>(findOwnerById(ownerId).getRestaurants());
-    }
-
-    @Transactional
-    public void changeOwnerLoginPassword(Long ownerId, String loginPassword) {
-        findOwnerById(ownerId).changeLoginPassword(loginPassword);
-    }
-
-    @Transactional
-    public void changeOwnerLoginPassword(String loginId, String loginPassword) {
-        findOwnerByLoginId(loginId).changeLoginPassword(loginPassword);
+    public List<RestaurantFindResponse> findRestaurantsByOwnerId(Long ownerId) {
+        return findOwnerEntityById(ownerId).getRestaurants().stream()
+            .map(restaurantMapper::toFindResponseDto)
+            .collect(Collectors.toList());
     }
 
     @Transactional
-    public void changeOwnerName(Long ownerId, String name) {
-        findOwnerById(ownerId).changeName(name);
-    }
-
-    @Transactional
-    public void changePhoneNumber(Long ownerId, String phoneNumber) {
-        findOwnerById(ownerId).changePhoneNumber(phoneNumber);
-    }
-
-    @Transactional
-    public void deleteOwner(Long ownerId) {
+    public void deleteOwnerById(Long ownerId) {
         ownerRepository.deleteById(ownerId);
     }
 
     @Transactional
-    public void registerRestaurant(Long ownerId, Long restaurantId) {
-        Owner owner = findOwnerById(ownerId);
-        Restaurant restaurant = restaurantService.findRestaurantById(restaurantId);
+    public void updateOwnerById(Long ownerId, OwnerUpdateRequest ownerUpdateRequest) {
+        Owner owner = findOwnerEntityById(ownerId);
+        ownerMapper.updateEntity(ownerUpdateRequest, owner);
+    }
+
+    @Transactional
+    public void registerRestaurant(Long ownerId, RestaurantCreateRequest restaurantCreateRequest) {
+        Owner owner = findOwnerEntityById(ownerId);
+        Long restaurantId = restaurantService.createRestaurant(restaurantCreateRequest);
+        Restaurant restaurant = restaurantService.findRestaurantEntityById(restaurantId);
 
         owner.addRestaurant(restaurant);
     }
 
     @Transactional
     public void removeRestaurant(Long ownerId, Long restaurantId) {
-        Owner owner = findOwnerById(ownerId);
+        Owner owner = findOwnerEntityById(ownerId);
         Restaurant restaurant = restaurantService.findRestaurantById(restaurantId);
 
         owner.getRestaurants().remove(restaurant);
+    }
+
+    public Owner findOwnerEntityById(Long ownerId) {
+        return ownerRepository.findById(ownerId)
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사장님 아이디입니다."));
     }
 
 }
