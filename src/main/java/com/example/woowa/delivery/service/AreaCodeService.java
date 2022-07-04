88760ba79b6e0d7c7596eee8097ba657188ea58f
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-07-04 17:03:49 +0900

    feat : service 추가

--- src/main/java/com/example/woowa/delivery/service/AreaCodeService.java
+++ src/main/java/com/example/woowa/delivery/service/AreaCodeService.java
@@ -1,13 +1,13 @@
 package com.example.woowa.delivery.service;
 
+import com.example.woowa.common.exception.ErrorMessage;
 import com.example.woowa.common.util.FileUtil;
 import com.example.woowa.delivery.entity.AreaCode;
 import com.example.woowa.delivery.repository.AreaCodeRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,17 +16,23 @@ public class AreaCodeService {
 
     private final AreaCodeRepository areaCodeRepository;
 
+    public AreaCode findEntityById(Long id) {
+        return areaCodeRepository.findById(id).orElseThrow(
+            () -> new RuntimeException(ErrorMessage.NOT_FOUND_AREA_CODE_ID.getMessage()));
+    }
+
     @Transactional
     public void deleteAll() {
         areaCodeRepository.deleteAll();
-        return;
     }
+
     public List<AreaCode> findAll() {
         return areaCodeRepository.findAll();
     }
 
     public AreaCode findByAddress(String defaultAddress) {
-        return areaCodeRepository.findByDefaultAddress(defaultAddress).orElseThrow(() -> new RuntimeException("없는 행정구역 입니다."));
+        return areaCodeRepository.findByDefaultAddress(defaultAddress)
+            .orElseThrow(() -> new RuntimeException("없는 행정구역 입니다."));
     }
 
     @Transactional
@@ -36,6 +42,7 @@ public class AreaCodeService {
     }
 
     public AreaCode findByCode(String code) {
-        return areaCodeRepository.findByCode(code).orElseThrow(() -> new RuntimeException("없는 행정구역 입니다."));
+        return areaCodeRepository.findByCode(code)
+            .orElseThrow(() -> new RuntimeException("없는 행정구역 입니다."));
     }
 }
--- src/main/java/com/example/woowa/delivery/service/DeliveryService.java
+++ src/main/java/com/example/woowa/delivery/service/DeliveryService.java
@@ -1,35 +1,91 @@
 package com.example.woowa.delivery.service;
 
-import com.example.woowa.delivery.entity.AreaCode;
+import com.example.woowa.common.exception.ErrorMessage;
+import com.example.woowa.delivery.dto.DeliveryCreateRequest;
+import com.example.woowa.delivery.dto.DeliveryResponse;
 import com.example.woowa.delivery.entity.Delivery;
 import com.example.woowa.delivery.entity.Rider;
 import com.example.woowa.delivery.enums.DeliveryStatus;
+import com.example.woowa.delivery.mapper.DeliveryMapper;
 import com.example.woowa.delivery.repository.DeliveryRepository;
+import com.example.woowa.order.order.entity.Order;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class DeliveryService {
 
     private final DeliveryRepository deliveryRepository;
 
-    public Long save(Delivery delivery) {
+    private final DeliveryMapper deliveryMapper;
+
+    private final RiderService riderService;
+
+    @Transactional
+    public Long save(DeliveryCreateRequest deliveryCreateRequest) {
+        // 작업
+        Delivery delivery = deliveryMapper.toEntity(deliveryCreateRequest);
         deliveryRepository.save(delivery);
         return delivery.getId();
     }
 
-    public List<Delivery> findAll() {
-        return deliveryRepository.findAll();
+    public Page<DeliveryResponse> findWaitingDelivery(PageRequest pageRequest) {
+        return deliveryRepository.findByDeliveryStatus(pageRequest, DeliveryStatus.DELIVERY_WAITING)
+            .map(deliveryMapper::toResponse);
     }
 
-    public List<Delivery> findByDeliveryStatus(DeliveryStatus deliveryStatus) {
-        return deliveryRepository.findByDeliveryStatus(deliveryStatus);
+    public Delivery findEntityById(Long id) {
+        return deliveryRepository.findById(id)
+            .orElseThrow(
+                () -> new RuntimeException(ErrorMessage.NOT_FOUND_DELIVERY_ID.getMessage()));
     }
 
-    public void acceptDelivery(Rider rider, Delivery delivery) {
+    public DeliveryResponse findResponseById(Long id) {
+        Delivery delivery = findEntityById(id);
+        return deliveryMapper.toResponse(delivery);
+    }
 
+    /**
+     * 추가할 사항 : 동시성 반영.
+     */
+    @Transactional
+    public void acceptDelivery(Long deliveryId, Long riderId, int deliveryMinute, int cookMinute) {
+        Delivery delivery = findEntityById(deliveryId);
+        if (!delivery.getDeliveryStatus().equals(DeliveryStatus.DELIVERY_WAITING)) {
+            throw new RuntimeException(ErrorMessage.ALREADY_RECEIVE_DELIVERY.getMessage());
+        }
+        Rider rider = riderService.findEntityById(riderId);
+        delivery.accept(rider, deliveryMinute, cookMinute);
+    }
+
+    @Transactional
+    public void delay(Long id, int delayMinute) {
+        Delivery delivery = findEntityById(id);
+        delivery.delay(delayMinute);
+    }
+
+    @Transactional
+    public void pickUp(Long id) {
+        Delivery delivery = findEntityById(id);
+        delivery.pickUp(30);
+    }
+
+    @Transactional
+    public void finish(Long id) {
+        Delivery delivery = findEntityById(id);
+        delivery.finish();
+    }
+
+    @Transactional
+    public Long createDelivery(Order order, String restaurantAddress, String customerAddress,
+        int deliveryFee) {
+        Delivery delivery = Delivery.createDelivery(order, restaurantAddress, customerAddress,
+            deliveryFee);
+        return deliveryRepository.save(delivery).getId();
     }
 }
