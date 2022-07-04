7f1ba45760ee9d5984e74d72201c5a4634a2b903
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-07-04 17:01:23 +0900

    feat : 배달 연관관계, 배달지역 추가

--- src/main/java/com/example/woowa/delivery/entity/Rider.java
+++ src/main/java/com/example/woowa/delivery/entity/Rider.java
@@ -1,13 +1,20 @@
 package com.example.woowa.delivery.entity;
 
 import com.example.woowa.common.base.BaseLoginEntity;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "rider")
@@ -15,23 +22,25 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Rider extends BaseLoginEntity {
 
+    @OneToMany
+    private final List<Delivery> deliveryList = new ArrayList<>();
+    @OneToMany(mappedBy = "rider", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<RiderAreaCode> riderAreaCodeList = new ArrayList<>();
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(columnDefinition = "BOOLEAN DEFAULT FALSE")
     private Boolean isDelivery;
 
-    @OneToMany
-    private List<Delivery> deliveryList = new ArrayList<>();
-
-    @OneToMany(mappedBy = "rider", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<RiderAreaCode> riderAreaCodeList = new ArrayList<>();
-
-    public Rider(String loginId, String loginPassword, String name, String phoneNumber) {
-        super(loginId, loginPassword);
-        //super(loginId, loginPassword, name, phoneNumber);
+    @Builder
+    private Rider(String loginId, String loginPassword, String name, String phoneNumber) {
+        super(loginId, loginPassword, name, phoneNumber);
         this.isDelivery = false;
+    }
+
+    public static Rider createRider(String loginId, String loginPassword, String name,
+        String phoneNumber) {
+        return new Rider(loginId, loginPassword, name, phoneNumber);
     }
 
     public void changeIsDelivery(boolean isDelivery) {
@@ -41,8 +50,15 @@ public class Rider extends BaseLoginEntity {
     public void addDelivery(Delivery delivery) {
         this.deliveryList.add(delivery);
     }
+
+
+    public void removeRiderAreaCode(RiderAreaCode riderAreaCode) {
+        if (riderAreaCodeList.contains(riderAreaCode)) {
+            this.riderAreaCodeList.remove(riderAreaCode);
+        }
+    }
+
     public void addRiderAreaCode(RiderAreaCode riderAreaCode) {
         this.riderAreaCodeList.add(riderAreaCode);
     }
-
 }
