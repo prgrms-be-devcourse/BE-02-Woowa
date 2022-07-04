417f87339d12af7cf91ebb34073719a1d4a05c9c
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-07-04 17:02:51 +0900

    feat : Mapper 클래스 추가

---
+++ src/main/java/com/example/woowa/delivery/mapper/RiderMapper.java
@@ -0,0 +1,22 @@
+package com.example.woowa.delivery.mapper;
+
+import com.example.woowa.delivery.dto.RiderCreateRequest;
+import com.example.woowa.delivery.dto.RiderResponse;
+import com.example.woowa.delivery.dto.RiderUpdateRequest;
+import com.example.woowa.delivery.entity.Rider;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface RiderMapper {
+
+    @Mapping(source = "password", target = "loginPassword")
+    Rider toRider(final RiderCreateRequest riderCreateRequest);
+
+    @Mapping(target = "riderAreaList", expression = "java(rider.getRiderAreaCodeList().stream().map(data -> data.getAreaCode().getDefaultAddress()).collect(java.util.stream.Collectors.toList()))")
+    RiderResponse toResponse(final Rider rider);
+
+    Rider toRider(final RiderUpdateRequest riderUpdateRequest);
+
+
+}
