e43355aee3ddd38d6cb417bee74f53256538d8fb
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-07-06 23:52:44 +0900

    feat : 테스트 환경 jpaAuditing 클래스 파일로 분리

--- src/main/java/com/example/woowa/WoowaApplication.java
+++ src/main/java/com/example/woowa/WoowaApplication.java
@@ -2,14 +2,12 @@ package com.example.woowa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class WoowaApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(WoowaApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(WoowaApplication.class, args);
+    }
 
 }
