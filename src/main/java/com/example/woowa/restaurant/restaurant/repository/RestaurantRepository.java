30898dfa7e89890a4e8e386e6c1dab1bea14c979
chlwlgns524 <chlwlgns524@naver.com>
2022-07-06 16:52:01 +0900

    feat: 가게 처리 관련 쿼리 메소드 추가

--- src/main/java/com/example/woowa/restaurant/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/example/woowa/restaurant/restaurant/repository/RestaurantRepository.java
@@ -1,11 +1,24 @@
 package com.example.woowa.restaurant.restaurant.repository;
 
+import com.example.woowa.restaurant.owner.entity.Owner;
 import com.example.woowa.restaurant.restaurant.entity.Restaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
     List<Restaurant> findByName(String name);
+
+    List<Restaurant> findByOwner(Owner owner);
+
+    @Query("SELECT r FROM Restaurant r WHERE r.id in "
+        + "(SELECT ra.restaurant.id FROM RestaurantAdvertisement ra WHERE ra.advertisement.id = :advertisementId)")
+    List<Restaurant> findByAdvertisementId(@Param("advertisementId") Long advertisementId);
+
+    @Query("SELECT r FROM Restaurant r WHERE r.id in "
+        + "(SELECT rc.restaurant.id FROM RestaurantCategory rc WHERE rc.category.id = :categoryId)")
+    List<Restaurant> findByCategoryId(@Param("categoryId") Long categoryId);
+
 }
