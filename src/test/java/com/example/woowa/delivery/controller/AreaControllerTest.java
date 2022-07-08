904aacbf0e1057eb545dadb02fcfdd8edad58f13
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-07-08 14:39:07 +0900

    test : AreaCodeController test

---
+++ src/test/java/com/example/woowa/delivery/controller/AreaControllerTest.java
@@ -0,0 +1,53 @@
+package com.example.woowa.delivery.controller;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.example.woowa.delivery.service.AreaCodeService;
+import com.example.woowa.security.configuration.SecurityConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureRestDocs
+@WebMvcTest(value = AreaCodeController.class, excludeFilters = {
+    @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = SecurityConfig.class
+    ),
+})
+@MockBean(JpaMetamodelMappingContext.class)
+@WithMockUser
+public class AreaControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    AreaCodeService areaCodeService;
+
+    @Test
+    @DisplayName("지역코드를 초기화할 수 있다.")
+    public void init() throws Exception {
+
+        mockMvc.perform(post("/api/v1/areaCode").with(csrf().asHeader()))
+            .andExpect(status().is2xxSuccessful())
+            .andDo(print())
+            .andDo(document("init-areacode"));
+    }
+}
